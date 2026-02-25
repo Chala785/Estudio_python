e397b8e1730cae9cf99a9f54622c928b0498ae88
Chala785 <juan_gallego82241@elpoli.edu.co>
2026-02-25 15:50:16 -0500

    Primer archivo de estudio

---
+++ 00_estudio.py
@@ -0,0 +1,12 @@
+# estos archivos es para
+# aprender python desde cero
+
+""" -------- Hola mundo --------  """
+print("hola mundo")
+
+# Consultar el tipo de dato
+print(type("hola mundo")) #  tipo 'str' 
+print(type(3)) # tipo 'int'
+print(type(3.14)) # tipo 'float'
+print(type(True)) # tipo 'bool'
+print(type(1+2j)) # tipo 'complex'
