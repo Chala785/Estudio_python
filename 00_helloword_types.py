707bfafe076c710a9457006f0d70ca932f0e890e
Chala785 <juan_gallego82241@elpoli.edu.co>
2026-02-25 21:38:07 -0500

    Hola mundo y tipos de datos

---
+++ 00_helloword_types.py
@@ -0,0 +1,13 @@
+# estos archivos es para
+# aprender python desde cero
+
+""" -------- Hola mundo --------  """
+print("hola mundo")
+
+# Consultar el tipo de dato
+print(type("hola mundo")) #  tipo 'str' 
+print(type(3)) # tipo 'int'
+print(type(3.14)) # tipo 'float'
+print(type(True)) # tipo 'bool'
+print(type(1+2j)) # tipo 'complex'
+print(type(print("Mi cadena de texto"))) # tipo 'NoneType' porque print no devuelve nada, solo imprime en pantalla
